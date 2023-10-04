057689a125fa4b12b026a1d6aa994ddd9bcb40fa
shriansh04 <140309798+shriansh04@users.noreply.github.com>
2023-10-04 17:40:26 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -2,25 +2,23 @@ const namedata = document.querySelector("[name-template]")
 const namecontainer = document.querySelector("[name-container]")
 const searching = document.querySelector("[searching]")
 let users = []
+
 searching.addEventListener("input", (e) => {
-    const  value  = e.target.value.toLowerCase()
-    users.forEach(  user => {
-        a = user.name.toLowerCase()
-        const ispresent = a.includes(value)
-        user.element.classList.toggle("hide",!ispresent)
-})
-}
-)
+    const value = e.target.value.toLowerCase();
+    users.forEach(user => {
+        const userName = user.name.toLowerCase();
+        const isPresent = userName.includes(value);
+        user.element.classList.toggle("hide", !isPresent);
+    });
+});
 
 fetch('https://dummyjson.com/users?limit=0&select=firstName')
-.then(res => res.json())
-.then(data => {
-    users = data.users.map(name => 
-    {
-        const card = namedata.content.cloneNode(true).children[0]
-        card.textContent = name.firstName
-        namecontainer.append(card)
-        return {name:name.firstName,element:card}
-    })
-})
-
+    .then(res => res.json())
+    .then(data => {
+        users = data.users.map(name => {
+            const card = namedata.content.cloneNode(true).children[0]
+            card.textContent = name.firstName
+            namecontainer.append(card)
+            return { name: name.firstName, element: card }
+        })
+    });
